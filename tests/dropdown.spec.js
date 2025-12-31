9005c44c98145635d42b9c9235ececdebaf2b011
Neelima <neelutgd507@gmail.com>
2025-12-30 22:00:13 -0800

    commit from main

--- tests/dropdown.spec.js
+++ tests/dropdown.spec.js
@@ -16,7 +16,7 @@ test('dropdown', { tag: '@drop' }, async ({ page }) => {
     }
     await dropdownCountry.selectOption({ index: 10 });
     await dropdownCountry.selectOption({ value: 'AF' });
-    console.log("end")
+    console.log("end *******")
 
 
 
